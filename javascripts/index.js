26c48bd4d76a5f345d0f3ef04f83d6ff69be7ea3
3Mustard <m23.cullen@gmail.com>
2020-01-15 14:21:49 -0500

    test passed: js fetches from rails api

---
+++ javascripts/index.js
@@ -0,0 +1,6 @@
+const BACKEND_URL = 'http://localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
+
+  console.log("Test passed")
