3d3a776936ad526c9e9142d717ca1cc4de695a77
Mustard <48289475+3Mustard@users.noreply.github.com>
2020-02-01 10:09:29 -0500

    add comments

--- javascripts/models/deck.js
+++ javascripts/models/deck.js
@@ -1,17 +1,21 @@
+//act's as a repository for instances of Card objects
 class Deck {
     constructor(){
-        this.cards = []
+        this.cards = [] //stores card objects
     }
-
+    
+    //add card object to a deck
     add(card){
         this.cards.push(card);
     }
-
+    
+    //return a card with a given id
     get(id){
         let index = id - 1;
         return this.cards[index]
     }
-
+    
+    //return a random card object from a 78 card deck
     draw(){
         let cardNumber = Math.round(Math.random() * 77);
         return this.cards[cardNumber];
