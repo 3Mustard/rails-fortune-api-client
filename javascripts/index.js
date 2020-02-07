b421b6feed07c8d2f6e7234285fb8b733a5d9cb2
Mustard <48289475+3Mustard@users.noreply.github.com>
2020-02-07 12:34:54 -0500

    add comments

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,10 +1,12 @@
 const BACKEND_URL = 'http://localhost:3000';
 let deck = new Deck();
 
+//Once dom loads invoke setupPage
 document.addEventListener('DOMContentLoaded', function(){
   setupPage();
 })
 
+//fetches card data and renders menu options to the user
 function setupPage(){
   Card.create();
   Menu.render();
